ea8f26eb4d9c3422a8af4a9af3e55400d86849f5
nikhil78957 <nikhil78957@gmail.com>
2026-01-16 18:51:38 +0530

    Update OpenRouter API key in config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // DO NOT commit this file to version control
 
 // OpenRouter API Key
-define('OPENROUTER_API_KEY', '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');
+define('OPENROUTER_API_KEY', 'add key');
 
 // TomTom API Key
 define('TOMTOM_API_KEY', 'ad6D4bTm8n9Z1f8Hv9HYhdINHi7fSOgA');
@@ -11,4 +11,5 @@ define('TOMTOM_API_KEY', 'ad6D4bTm8n9Z1f8Hv9HYhdINHi7fSOgA');
 // Optional: Add other configuration settings here
 define('API_TIMEOUT', 30); // seconds
 define('MAX_TOKENS', 500);
+
 ?>
